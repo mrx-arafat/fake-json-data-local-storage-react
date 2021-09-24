3a5a779c9f637b9577ef67300a662d100881fdc3
Easin Arafat <arafatmrx@gmail.com>
2021-09-24 21:11:29 +0600

    local storage

--- src/utilities/fakedb.js
+++ src/utilities/fakedb.js
@@ -1,13 +1,23 @@
 //using local storage as DB
 
 const addToFakeDb = (id) => {
-  const exist = localStorage.getItem(id);
+  const exist = localStorage.getItem("shopping_cart");
+
+  // here at first I used const : be alart
+  let shopping_cart = {};
 
   if (!exist) {
-    localStorage.setItem(id, 1);
+    shopping_cart[id] = 1;
   } else {
-    const newCount = parseInt(exist) + 1;
-    localStorage.setItem(id, newCount);
+    shopping_cart = JSON.parse(exist);
+
+    //check if id available in shopping cart
+    if (shopping_cart[id]) {
+      const newCount = shopping_cart[id] + 1;
+      shopping_cart[id] = newCount;
+    } else {
+      shopping_cart[id] = 1;
+    }
   }
 };
 
