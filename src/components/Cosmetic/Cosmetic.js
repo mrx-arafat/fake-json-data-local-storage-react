8b727c58a3a8db445ecf3bebd4e68f0b305d17b8
Easin Arafat <arafatmrx@gmail.com>
2021-09-24 15:12:37 +0600

    init

--- src/components/Cosmetic/Cosmetic.js
+++ src/components/Cosmetic/Cosmetic.js
@@ -1,7 +1,16 @@
 import React from "react";
 
-const Cosmetic = () => {
-  return <div></div>;
+const Cosmetic = (props) => {
+  console.log(props.cosmetic);
+
+  const { name, age, _id } = props.cosmetic;
+  return (
+    <div>
+      <h4>id:{_id}</h4>
+      <h2>name:{name}</h2>
+      <p>age: {age}</p>
+    </div>
+  );
 };
 
 export default Cosmetic;
--- src/components/Cosmetics/Cosmetics.js
+++ src/components/Cosmetics/Cosmetics.js
@@ -1,15 +1,19 @@
 import React, { useEffect, useState } from "react";
+import Cosmetic from "../Cosmetic/Cosmetic";
 
 const Cosmetics = () => {
   const [cosmetics, setCosmetics] = useState([]);
   useEffect(() => {
     fetch("./cosmetics.json")
       .then((res) => res.json())
-      .then((data) => console.log(data));
+      .then((data) => setCosmetics(data));
   }, []);
 
   return (
     <div>
+      {cosmetics.map((cosmetic) => (
+        <Cosmetic cosmetic={cosmetic}></Cosmetic>
+      ))}
       <h1>hello </h1>
     </div>
   );
